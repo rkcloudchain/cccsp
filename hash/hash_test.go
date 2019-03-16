e8f6501a3a19486d9dd951ec2342f4eb8b521ea2
Xu Qiaolun <jamesxql@gmail.com>
2019-03-17 02:06:07 +0800

    Add hash test case

---
+++ hash/hash_test.go
@@ -0,0 +1,23 @@
+package hash
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/sha3"
+)
+
+func TestHash(t *testing.T) {
+	h := NewHasher(sha3.New256)
+	msg := []byte("Hello World")
+	out, err := h.Hash(msg)
+	assert.NoError(t, err)
+
+	h2 := sha3.New256()
+	h2.Write(msg)
+	out2 := h2.Sum(nil)
+	assert.Equal(t, out2, out)
+
+	hf := h.GetHash()
+	assert.Equal(t, sha3.New256(), hf)
+}
